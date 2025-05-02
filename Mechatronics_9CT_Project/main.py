9f095fab385c5e8f42c17e1ed32ff64207d51681
fjdksa81 <charles.mcdonagh@education.nsw.gov.au>
2025-05-02 12:27:56 +1000

    Colour programming.
    Added functionality to the colour sensor.

--- Mechatronics_9CT_Project/main.py
+++ Mechatronics_9CT_Project/main.py
@@ -8,6 +8,7 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 sensor_motor = Motor(Port.A)
+colour_sensor = ColorSensor(Port.S3)
 
 """Used to test the movement of the colour sensor's motor, using run_angle to move the motor at 
 a speed of 90 degrees per second for 90 degrees, to stop the motor from hitting the ground or the wheels.
@@ -15,3 +16,16 @@ It then moves it back to it's previous posiiton and corrects the movements."""
 
 sensor_motor.run_angle(90, -90)
 sensor_motor.run_angle(90, 90)
+
+"""Tests the colour sensor, to detect the two good blocks (red or yellow), making one beep
+unrecognised blocks or undetect makes two beeps."""
+
+
+if colour_sensor.color() == Color.RED or Color.YELLOW:
+    #pick up here
+    ev3.speaker.beep()
+
+else:
+    #move back here and turn 90 degrees
+    speaker.beep()
+    speaker.beep()
